88b1d7ad163d41e04bc9f4bca3546cfda8ba8fc5
fxkjd <6142041+fxkjd@users.noreply.github.com>
2018-05-17 12:30:27 +0200

    Fix zsh provision script

--- odin/provision/zsh.sh
+++ odin/provision/zsh.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Installing Go"
+echo "Installing Oh My ZSH"
 
-VERSION=1.10
-OS=linux-amd64
-
-which go
+which zsh 
 RESULT=$?
 
 if [ $RESULT -eq 0 ]; then
-    echo "Go already installed"
+    echo "ZSH already installed"
 else
-    echo "Version: go$VERSION.$OS"
-    wget -q https://dl.google.com/go/go$VERSION.$OS.tar.gz -O /tmp/go$VERSION.$OS.tar.gz
-    sudo tar -C /usr/local -xzf /tmp/go$VERSION.$OS.tar.gz
-    rm -f /tmp/go$VERSION.$OS.tar.gz
-    echo "PATH=\$PATH:/usr/local/go/bin" >> ~/.bash_profile
-    echo "PATH=\$PATH:/usr/local/go/bin" >> ~/.zshrc
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc 
 fi
